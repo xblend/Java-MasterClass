a2326f76c5eb6a702e20137fcb7874526c3cf87d
Akshat Marathe <marathe.akshat@live.com>
2022-08-05 09:14:02 +0530

    Added files for ScopeChallenge

---
+++ ScopeChallenge/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int x(){
+        Scanner x = new Scanner(System.in);
+        return x.nextInt();
+    }
+    public static void main(String[] args) {
+        X x = new X(x());
+        x.x();
+    }
+}
---
+++ ScopeChallenge/src/com/company/X.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class X {
+    private int x;
+
+    public X(int x) {
+        this.x = x;
+    }
+    public void x(){
+        for(int x=0;x<13;x++)
+            System.out.println(this.x+" times "+x+" equals "+this.x*x);
+    }
+}
