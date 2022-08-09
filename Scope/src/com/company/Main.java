23203e230b22a08bf73cc2433ed6c84faa929fca
Akshat Marathe <marathe.akshat@live.com>
2022-08-09 10:10:15 +0530

    Added files for Scope

--- Scope/src/com/company/Main.java
+++ Scope/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scope scope = new Scope();
+        scope.multiply();
+        scope.multiplyLocalVarOne();
+
+        Scope.InnerClass innerClass = scope.new InnerClass();
+        innerClass.multiply();
+        innerClass.multiplyOuterVar();
+        scope.multiplyInnerVar();
     }
 }
--- Scope/src/com/company/Scope.java
+++ Scope/src/com/company/Scope.java
@@ -3,23 +3,45 @@ package com.company;
 public class Scope {
     private int varOne = 1;
 
-    private int getVarOne(){
+    private int getVarOne() {
         return this.varOne;
     }
-    public void multiply(){
-        System.out.println("This is using declared member varOne of Scope class");
-        for (int i=0;i<111;i++)
-            System.out.println(varOne+" times "+i+" equals "+varOne*i);
+
+    public void multiply() {
+        System.out.println("\nThis is using declared member varOne of Scope class");
+        for (int i = 0; i < 11; i++)
+            System.out.println(varOne + " times " + i + " equals " + varOne * i);
     }
-    public void multiplyLocalVarOne(){
+
+    public void multiplyLocalVarOne() {
         int varOne = 2;
-        System.out.println("This is using locally declared variable varOne");
-        for(int i=0;i<11;i++)
-            System.out.println(varOne+" times "+i+" equals "+varOne*i);
+        System.out.println("\nThis is using locally declared variable varOne");
+        for (int i = 0; i < 11; i++)
+            System.out.println(varOne + " times " + i + " equals " + varOne * i);
     }
-    public class InnerClass{
+    public void multiplyInnerVar() {
+        InnerClass innerClass = new InnerClass();
+        System.out.println("\nThis is using declared member varOne of InnerClass (an inner class in Scope class) " +
+                "\n accessed from outer class");
+        for (int i = 0; i < 11; i++)
+            System.out.println(innerClass.varOne + " times " + i + " equals " + innerClass.varOne * i);
+    }
+
+    public class InnerClass {
         private int varOne = 3;
+
+        public void multiply() {
+            System.out.println("\nThis is using declared member varOne of InnerClass (an inner class in Scope class)");
+            for (int i = 0; i < 11; i++)
+                System.out.println(varOne + " times " + i + " equals " + varOne * i);
+        }
+
+        public void multiplyOuterVar() {
+            Scope scope = new Scope();
+            System.out.println("\nThis is using declared member varOne of Scope class from inner class");
+            for (int i = 0; i < 11; i++)
+                //Inner class can access the private members of its outer class
+                System.out.println(scope.varOne + " times " + i + " equals " + scope.varOne * i);
+        }
     }
-
-
 }
