c06387d8b1ec5c79f676966250527fe07080ef5a
Akshat Marathe <marathe.akshat@live.com>
2022-08-09 09:44:08 +0530

    Added a comment for the default access of the member variables and the methods of an Interface

--- Interfaces/src/com/company/IApp.java
+++ Interfaces/src/com/company/IApp.java
@@ -2,6 +2,7 @@ package com.company;
 
 //The methods in the interface are by default public
 //The variables in the interface are by default public static final therefore it's a must to initialize them
+//There can't be any other access modifiers other than public which can be assigned to the members of the interface
 
 public interface IApp {
     int MAX_APPS=25;
